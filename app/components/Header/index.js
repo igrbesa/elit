9212d9b9367e0de6ad595ef39f3b2f9147478cab
Igor Grbeša <igor.m.grbesa@gmail.com>
2017-08-22 22:46:07 +0200

    drawer

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import NavLink from 'components/NavLink';
 // import Logo from './Logo';
@@ -8,9 +8,11 @@ import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/Button';
 import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
+import Button from 'material-ui/Button';
 import MoreVertIcon from 'material-ui-icons/MoreVert';
 import MenuIcon from 'material-ui-icons/Menu';
 import Menu, { MenuItem } from 'material-ui/Menu';
+import { Drawer } from 'material-ui';
 import Typography from 'material-ui/Typography';
 import { Route, Link } from 'react-router-dom';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
@@ -20,6 +22,9 @@ const styleSheet = createStyleSheet({
     marginTop: 30,
     width: '100%'
   },
+  flex: {
+    flex: 1,
+  }
 });
 
 const options = [
@@ -29,39 +34,106 @@ const options = [
   'Dione'
 ];
 
-function header(props) {
+class Header extends Component {
 
-  const onTouchTap = (e) => {
-    alert(props);
+  constructor(props, context) {
+    super(props, context);
+    this.handleDrawOpening = this.handleDrawOpening.bind(this);
+    this.handleDrawerClosing = this.handleDrawerClosing.bind(this);
+    this.state = {
+      openedDrawer: false
+    }
   }
 
-  const classes = props.classes;
+  handleDrawOpening() {
+    this.setState({ openedDrawer: true });
+  }
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="fixed" color="accent">
-        <Toolbar>
-          <IconButton color="contrast" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>
-            Title
+  handleDrawerClosing() {
+    this.setState({ openedDrawer: false });
+  }
+
+  render() {
+
+    const classes = this.props.classes;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="fixed" color="accent">
+          <Toolbar>
+            <IconButton color="contrast" aria-label="Menu">
+              <MenuIcon onClick={this.handleDrawOpening} />
+              <Drawer
+                open={this.state.openedDrawer}
+                onRequestClose={this.handleDrawerClosing}
+                onClick={this.handleDrawerClosing}
+              >
+                fdsf
+
+              N</Drawer>
+            </IconButton>
+            <Typography type="title" color="inherit" className={classes.flex}>
+              Title
           </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-    /*         
-             <FlatButton label="Client" onTouchTap={onTouchTap}>
-               </FlatButton> */
-    // <div className={styles.header}>
-    //   <NavLink to="/budget" label="Budget" styles={styles} />
-    //   <NavLink to="/reports" label="Reports" styles={styles} />
-    //   <NavLink to="/material" label="Material test" styles={styles} />
-    //   <GitHubButton className={styles.gitHubButton} type="Star" />
-    //   <GitHubButton className={styles.gitHubButton} type="Fork" />
-    //   <Logo />
-    // </div>
-  )
+            <Button color="contrast">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+
 };
 
-export default withStyles(styleSheet)(header);
+export default withStyles(styleSheet)(Header);
+
+//*************************** */
+
+
+// function header(props) {
+
+//   const onTouchTap = (e) => {
+//     alert(props);
+//   }
+
+//   const classes = props.classes;
+
+//   const handleLeftOpen = () => toggleDrawer('left', true);
+
+//   const toggleDrawer = (side, open) => {
+//     const drawerState = {};
+//     drawerState[side] = open;
+//     this.setState({ open: drawerState });
+//   };
+
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="fixed" color="accent">
+//         <Toolbar>
+//           <IconButton color="contrast" aria-label="Menu">
+//             <MenuIcon onClick={handleLeftOpen} />
+//           </IconButton>
+//           <Typography type="title" color="inherit" className={classes.flex}>
+//             Title
+//           </Typography>
+//           <Button color="contrast">Login</Button>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//     /*         
+//              <FlatButton label="Client" onTouchTap={onTouchTap}>
+//                </FlatButton> */
+//     // <div className={styles.header}>
+//     //   <NavLink to="/budget" label="Budget" styles={styles} />
+//     //   <NavLink to="/reports" label="Reports" styles={styles} />
+//     //   <NavLink to="/material" label="Material test" styles={styles} />
+//     //   <GitHubButton className={styles.gitHubButton} type="Star" />
+//     //   <GitHubButton className={styles.gitHubButton} type="Fork" />
+//     //   <Logo />
+//     // </div>
+//   )
+// };
+
+// export default withStyles(styleSheet)(header);
--- app/containers/Client/index.js
+++ app/containers/Client/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { AppBar, Toolbar } from 'material-ui';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
 // import transactionReducer from 'modules/transactions';
 // import categoryReducer from 'modules/categories';
 // import { injectAsyncReducers } from 'store';
@@ -11,8 +13,46 @@ import React from 'react';
 //   categories: categoryReducer,
 // });
 
-const ClientContainer = () => (
-    <h1>Here goes Client</h1>
-);
+const styleSheet = createStyleSheet({
+    root: {
+        marginTop: 0,
+        width: '100%'
+    },
+    flex: {
+        flex: 1,
+    },
+    positionFixed: {
+        marginLeft: 40,
+        top: 80
+    }
+});
 
-export default ClientContainer;
+const ClientContainer = (props) => {
+
+    const { classes } = props;
+
+    return (
+
+        <div >
+            {/* <AppBar position="fixed" color="accent" className={classes.positionFixed}>
+                <Toolbar>
+                    <span>some text</span>
+                </Toolbar>
+            </AppBar> */}
+            <h1>Here goes Client1</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client</h1>
+            <h1>Here goes Client end</h1>
+        </div>
+    )
+};
+
+export default withStyles(styleSheet)(ClientContainer);
